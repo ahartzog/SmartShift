eed8a0c9f308a846dd50376968f6c2d304405eda
Alek <ahartzog@gmail.com>
2021-09-02 12:41:41 -0400

    More workspace setup

---
+++ Modules/src/Models/Employee.ts
@@ -0,0 +1,22 @@
+export interface Employee {
+    id: string;
+    firstName: string;
+    lastName: string;
+    emailAddress: string;
+}
+
+export enum Affinities {
+    Primary,
+    Secondary,
+    Fries,
+    Breading, 
+    Machine,
+    Prep,
+    Register,
+    Upstream,
+    Desserts,
+    Runner,
+    Bagger,
+    Drinks,
+    Tent
+}
