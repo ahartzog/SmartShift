9a980b858d5c2561d0325882331edbfe18d60c4f
Alek <ahartzog@gmail.com>
2021-09-02 13:39:16 -0400

    Set server to run on 3080

--- ss-web-app/src/App.tsx
+++ ss-web-app/src/App.tsx
@@ -4,17 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>I SAY GOOD DAY SIR</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
