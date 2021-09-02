200a715f0e067ec6d926f2d3a3556c9b3d901286
Alek <ahartzog@gmail.com>
2021-09-02 15:10:45 -0400

    Connecting to DB
    Password is in plain text; don't judge me

--- server-container/server/src/app.module.ts
+++ server-container/server/src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { EmployeeController } from './employee/employee.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Employee } from './employee/employee.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      port: 27017,
+      //username: 'root',
+      //password: 'alllowercasepassword',
+      database: 'SmartShift',
+      authSource: "admin",
+      url: "mongodb+srv://root:alllowercasepassword@cluster0.9kvv5.mongodb.net",
+      useNewUrlParser: true,
+      ssl: true,
+      entities: [Employee],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, EmployeeController],
   providers: [AppService],
 })
---
+++ server-container/server/src/employee/employee.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Employee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  emailAddress: boolean;
+}
