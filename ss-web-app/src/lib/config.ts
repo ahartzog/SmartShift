e6acb04cd0dd07d764ff7965a0435c8dde6f3112
Alek <ahartzog@gmail.com>
2021-09-04 05:15:04 -0400

    Add config files
    (Public)

--- ss-web-app/src/lib/config.ts
+++ ss-web-app/src/lib/config.ts
@@ -1,5 +1,12 @@
+// This pulls in values defined in /.deploy/.env.XXX, set by the build scripts. See README.md for details
+// Everything is pulled in via string, you must convert to bool/number as needed here
 const config = {
-  API_URL: 'http://localhost:3080/',
+  ENV: process.env.REACT_APP_ENV,
+  LOCAL_STORAGE_AUTH_KEY: process.env.REACT_APP_LOCAL_STORAGE_AUTH_KEY,
+  API_URL: process.env.REACT_APP_API_URL,
+  BUGSNAG_KEY: process.env.REACT_APP_BUGSNAG_KEY,
+  ALARM_FETCH_RESPONSE_MAX_TIME_MS:
+    process.env.REACT_APP_ALARM_FETCH_RESPONSE_MAX_TIME_MS,
 };
 
 export { config };
