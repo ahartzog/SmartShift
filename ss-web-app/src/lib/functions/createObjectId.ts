72db97df75dd17993f76b6e219aa7414334348f8
Alek Hartzog <ahartzog@gmail.com>
2021-09-08 21:01:59 -0400

    rename modules

---
+++ ss-web-app/src/lib/functions/createObjectId.ts
@@ -0,0 +1,12 @@
+// import ObjectID from "bson-objectid";
+// // import { ObjectId } from "mongodb";
+
+// interface MockObjectId extends ObjectID {
+//   generationTime: number;
+//   generate: (time?: number) => string;
+// }
+// const createObjectId = () => {
+//   return new ObjectID() as MockObjectId;
+// };
+
+// export { createObjectId };
