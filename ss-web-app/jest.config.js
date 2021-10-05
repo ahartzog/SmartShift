a830147d0114c67bcd6d332af9a6dbb60f0d3ca2
Alek Hartzog <ahartzog@gmail.com>
2021-10-04 21:58:36 -0400

    Testing commit

---
+++ ss-web-app/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  moduleNameMapper: {
+    '@bugsnag/js': '@bugsnag/browser',
+  },
+};
---
+++ ss-web-app/src/lib/utility/bugSnag/bugSnagService.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BugSnagService } from './bugSnagService';
+import type Config from 'lib/config';
+
+//Per https://github.com/bugsnag/bugsnag-js/issues/452#issuecomment-449418103
+//@ts-ignore
+setTimeout().__proto__.unref = function () {};
+
+const demoConfig = {
+  ENV: 'development',
+} as typeof Config;
+
+const bugSnagService = new BugSnagService(demoConfig);
+
+it('adds 1 + 2 to equal 3', () => {
+  const one = 1;
+  const two = 2;
+  expect(one + two).toBe(3);
+});
+
+it('console.logs breadcrumbs since we are in dev mode', () => {
+  bugSnagService.leaveBreadcrumb('Hello Breadcrumb World');
+  const consoleSpy = jest.spyOn(console, 'log');
+  expect(consoleSpy).toHaveBeenCalledWith('Hello Breadcrumb World');
+  // expect(console.log).toHaveBeenCalledWith('Hello Breadcrumb World');
+});
