4f5e2a295ba87fff664f1c59d061e23763105a98
Alek <ahartzog@gmail.com>
2021-10-25 18:55:50 -0400

    Python WIP

--- python-server/ssPythonServer/polls/models.py
+++ python-server/ssPythonServer/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
@@ -6,6 +9,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date_published')
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -15,7 +20,7 @@ class Choice(models.Model):
         return self.choice_text
 
 
-class AleksClass():
+class AleksClass(models.Model):
     name = "Alek"
     def getName(self):
-        return name
+        return self.name
