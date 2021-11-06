db6aa039680404c6d709ad21d6a9df32e091b013
Alek <ahartzog@gmail.com>
2021-11-06 07:18:44 -0400

    Map showing

--- ss-web-app/src/pages/MapBox/MapBox.tsx
+++ ss-web-app/src/pages/MapBox/MapBox.tsx
@@ -1,12 +1,17 @@
 import mapboxgl, { Map } from 'mapbox-gl';
-import React, { useEffect, useRef, useState } from 'react';
-
-import { MapBoxControls } from './MapBoxControls';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
+import axios from 'axios';
 
 require('./mapboxStyle.css');
 const MapBox = () => {
-  const mapContainer = useRef<HTMLDivElement>();
   const map = useRef<Map>(null);
+  const [mapInfo, setMapInfo] = useState(null);
+
+  const mapRef = useCallback((mapNode) => {
+    // console.log(catImageNode);
+    setMapInfo(mapNode);
+    // setCatInfo(catImageNode?.getBoundingClientRect());
+  }, []);
 
   useEffect(() => {
     console.log('Effect!');
@@ -27,12 +32,86 @@ const MapBox = () => {
 
   console.log('render!');
 
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  const round = (num: number) => {
+    return Math.round(num * 1e2) / 1e2;
+  };
+
+  const placesOfInterest = [
+    {
+      name: 'Our House',
+      address: '9212 NW 17th place, Gainesville FL 32606',
+    },
+    {
+      name: 'Grandma Lisa',
+      address: '3224 Pine Rd, Orange Park, FL',
+    },
+    {
+      name: 'Grandpa John',
+      address: '2410 Edward Rd, West Palm Beach FL 33410',
+    },
+  ];
+
+  const getGeoDataForAddress = async (searchString: string) => {
+    const homeGeo = await axios({
+      method: 'get',
+      url: `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchString}.json`,
+    });
+
+    console.log('home go?', homeGeo.data);
+    // new mapboxgl.Marker({
+    //   color: 'red',
+
+    // }).setLngLat
+  };
+
+  useEffect(() => {
+    console.log('Map info?', mapInfo);
+    //mapInfo.current!.addControl(new mapboxgl.NavigationControl());
+
+    // props.mapBoxRef.current!.on('move', () => {
+    //   setLng(round(props.mapBoxRef.current!.getCenter().lng));
+    //   setLat(round(props.mapBoxRef.current!.getCenter().lat));
+    //   setZoom(round(props.mapBoxRef.current!.getZoom()));
+    // });
+  }, [mapInfo]);
+
+  useEffect(() => {
+    console.log('running...');
+    placesOfInterest.forEach((place) => {
+      //https://docs.mapbox.com/mapbox-gl-js/example/add-a-marker/
+      getGeoDataForAddress(place.address);
+    });
+  });
+
   return (
-    <MapBoxControls mapBoxRef={map}>
+    <div>
+      <h1>Map, Map, Map!</h1>
+
+      <div className='sidebar'>
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
       {/* @ts-ignore */}
-      <div id='map' className='map-container' />
-    </MapBoxControls>
+      <div id='map' ref={mapRef} className='map-container' />
+      <div>
+        <h3>Find Places</h3>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          {placesOfInterest.map((pl) => {
+            return <button key={pl.name}>{pl.name}</button>;
+          })}
+        </div>
+      </div>
+    </div>
   );
+
+  // return (
+
+  //     {/* @ts-ignore */}
+
+  // );
 };
 
 export { MapBox };
